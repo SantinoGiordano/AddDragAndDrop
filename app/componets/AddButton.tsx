e9cf8bfc5f8f3f21d8a9ad606197d7eec673ef39
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-13 10:26:56 -0500

    added add user

---
+++ app/componets/AddButton.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { useState } from "react";
+
+interface Users {
+    id: number;
+    name: string;
+    age: Number;
+  }
+
+export default function AddButton() {
+
+
+
+
+  function addUser() {
+    const newUser = {
+      id: users.length + 1,
+      name: `New User ${users.length + 1}`,  
+      age: Math.floor(Math.random() * 50) + 20  
+    };
+
+    setUsers([...users, newUser]);
+
+  return (
+    <div>
+      <h1>Users List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} (ID: {user.id}, Age: {user.age})
+          </li>
+        ))}
+      </ul>
+      <button onClick={addUser}>Add</button>
+    </div>
+  );
+}
+
