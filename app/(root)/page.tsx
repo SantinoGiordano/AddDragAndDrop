384a3a7d8280d2ee0e327c1c9496f370745d0957
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-13 15:25:43 -0500

    added fragments and <hr>

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,11 @@
 
 export default function Home(){
     return(
-        <div className="m-20 text-center text-3xl">
+        <>
+        <div className=" mb-2 m-20 text-center text-3xl">
             Hello World
         </div>
+        <hr></hr>
+        </>
     )
 }
